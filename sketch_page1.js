4ce0d4e56732be8073f4ef249b22c2008358a993
Darek Fakorede <df2631@nyu.edu>
2026-03-25 19:58:42 -0400

    Misc code cleanup

--- sketch_page1.js
+++ sketch_page1.js
@@ -3,24 +3,21 @@ let angle = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  angleMode(DEGREES); 
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(220);
 
-  
   x = lerp(x, mouseX, 0.05);
   y = lerp(y, mouseY, 0.05);
-  angle = lerp(angle, mouseX, 0.05); 
+  angle = lerp(angle, mouseX, 0.05);
 
-  
   translate(x, y);
 
-  
   for (let i = 0; i < 4; i++) {
-    rotate(angle);       
-    rect(0, 0, 100, 15); 
-    translate(100, 0);   
+    rotate(angle);
+    rect(0, 0, 100, 15);
+    translate(100, 0);
   }
 }
--- sketch_page3.js
+++ sketch_page3.js
@@ -1,47 +1,39 @@
 let trail = [];
-
 let numSegments = 60;
 let wormWidth = 60;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  colorMode(HSB, 360, 100, 100, 1); 
+  colorMode(HSB, 360, 100, 100, 1);
   background(0);
 }
 
 function draw() {
   background(0, 0.05);
 
-  
   let mx = constrain(mouseX, 0, width);
   let my = constrain(mouseY, 0, height);
 
-  
-  let pos = { 
-    x: mx, 
-    y: my, 
-    c: frameCount % 360 
+  let pos = {
+    x: mx,
+    y: my,
+    c: frameCount % 360
   };
-  
+
   console.log(trail);
-  
+
   trail.push(pos);
 
   if (trail.length > numSegments) {
     trail.shift();
   }
 
- 
   for (let i = 0; i < trail.length; i++) {
     let segment = trail[i];
-    
-    
     let d = wormWidth * sin(map(i, 0, trail.length - 1, 0, PI));
-    
-    
-    fill(segment.c, 80, 100); 
-    noStroke();
 
+    fill(segment.c, 80, 100);
+    noStroke();
     drawMirrored(segment.x, segment.y, d);
   }
 }
